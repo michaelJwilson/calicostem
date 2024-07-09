171e9fd1ff52637485837fc141881b1eda655d7c
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-07-09 19:46:34 +0100

    fix

--- test.py
+++ test.py
@@ -11,20 +11,39 @@ def compute_nbinom_pmf_chunk(args):
     data_chunk, n, p = args
     return scipy.stats.nbinom.logpmf(data_chunk, n, p)
 
-def thread_nbinom(data, n, p, num_threads=NUM_THREADS):
-    executor = ThreadPoolExecutor(max_workers=NUM_THREADS)
+def compute_betabinom_pmf_chunk(args):
+    data_chunk, n, a, b = args
+    return scipy.stats.betabinom.logpmf(data_chunk, n, a, b)
+
+def thread_nbinom(k, n, p, num_threads=NUM_THREADS, executor=None):
+    if executor is None:
+        executor = ThreadPoolExecutor(max_workers=NUM_THREADS)
     
-    data_chunks = np.array_split(data, num_threads)
+    k_chunks = np.array_split(k, num_threads)
     n_chunks = np.array_split(n, num_threads)
     p_chunks = np.array_split(p, num_threads)
 
-    args = [xx for xx in zip(data_chunks, n_chunks, p_chunks)]
+    args = (xx for xx in zip(k_chunks, n_chunks, p_chunks))
     results = executor.map(compute_nbinom_pmf_chunk, args)
 
     return np.concatenate(list(results))
 
+def thread_betabinom(k, n, a, b, num_threads=NUM_THREADS, executor=None):
+    if executor	is None:
+        executor = ThreadPoolExecutor(max_workers=NUM_THREADS)
+    
+    k_chunks = np.array_split(k, num_threads)
+    n_chunks = np.array_split(n, num_threads)
+    a_chunks = np.array_split(a, num_threads)
+    b_chunks = np.array_split(b, num_threads)
+
+    args = (xx for xx in zip(k_chunks, n_chunks, a_chunks, b_chunks))
+    results = executor.map(compute_betabinom_pmf_chunk, args)
+
+    return np.concatenate(list(results))
+
 def get_mock_data():
-    NN = 4_000_000
+    NN = 100_000
     
     ns = 100 + np.arange(NN)
     ps = 0.5 * np.ones_like(ns)
@@ -48,6 +67,7 @@ def test_exp(mock_data, benchmark):
     def wrap_exp():
         return scipy.stats.nbinom.logpmf(ks, ns, ps)
 
+    benchmark.group = "nb"
     result = benchmark(wrap_exp)
 
     assert np.allclose(exp, result)
@@ -58,6 +78,7 @@ def test_exp_bb(mock_data, benchmark):
     def wrap_exp():
         return scipy.stats.betabinom.logpmf(ks, ns, aa, bb)
 
+    benchmark.group = "bb"
     result = benchmark(wrap_exp)
 
     assert np.allclose(exp_bb, result)
@@ -69,7 +90,8 @@ def test_rust(mock_data, benchmark):
     
     def wrap_rust():
         return core.nb(ks, ns, ps)
-        
+
+    benchmark.group = "nb"
     rust_result = benchmark(wrap_rust)
     
     assert np.allclose(exp, rust_result)
@@ -82,6 +104,7 @@ def test_rust_bb(mock_data, benchmark):
     def wrap_rust():
         return core.bb(ks, ns, aa, bb)
 
+    benchmark.group = "bb"
     rust_result = benchmark(wrap_rust)
 
     assert np.allclose(exp_bb, rust_result)
@@ -89,12 +112,28 @@ def test_rust_bb(mock_data, benchmark):
 def test_thread(mock_data, benchmark):
     ks, ns, ps, aa, bb, exp, exp_bb = mock_data
 
-    def wrap_thread():
-        return thread_nbinom(ks, ns, ps)
+    executor = ThreadPoolExecutor(max_workers=NUM_THREADS)
     
+    def wrap_thread():
+        return thread_nbinom(ks, ns, ps, executor=executor)
+
+    benchmark.group = "nb"
     thread_result = benchmark(wrap_thread)
 
     assert np.allclose(exp, thread_result)
+
+def test_thread_bb(mock_data, benchmark):
+    ks, ns, ps, aa, bb, exp, exp_bb = mock_data
+
+    executor = ThreadPoolExecutor(max_workers=NUM_THREADS)
+    
+    def wrap_thread():
+        return thread_betabinom(ks, ns, aa, bb, executor=executor)
+
+    benchmark.group = "bb"
+    result = benchmark(wrap_thread)
+
+    assert np.allclose(exp_bb, result)
     
 @line_profiler.profile
 def profile(ks, ns, ps, aa, bb, exp, exp_bb, iterations=10):
