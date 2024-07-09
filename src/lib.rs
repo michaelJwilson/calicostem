c9c909252dab66c04a843e27f21561c57e9802fa
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-07-09 20:02:58 +0100

    working

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,78 @@
 extern crate statrs;
 
-use statrs::{distribution::{Discrete, NegativeBinomial}};
+use numpy::{PyArrayDyn, PyReadonlyArray1};
 use pyo3::prelude::*;
-use numpy::{PyReadonlyArray1, PyArrayDyn};
 use rayon::prelude::*;
+use statrs::distribution::{Discrete, NegativeBinomial};
 use statrs::function::gamma::ln_gamma;
 
+// NB export RAYON_NUM_THREADS=12
+
+let batch_size = 1024;
+
 #[pyfunction]
-fn nb(k: PyReadonlyArray1<'_, f64>, n: PyReadonlyArray1<'_, f64>, p: PyReadonlyArray1<'_, f64>) -> PyResult<Vec<f64>> {
-    // NB export RAYON_NUM_THREADS=12
+fn nb(
+    k: PyReadonlyArray1<'_, f64>,
+    n: PyReadonlyArray1<'_, f64>,
+    p: PyReadonlyArray1<'_, f64>,
+) -> PyResult<Vec<f64>> {
+
     let k = k.to_vec()?;
     let n = n.to_vec()?;
     let p = p.to_vec()?;
 
-    let probs: Vec<f64> = n.par_iter().zip(k.par_iter()).zip(p.par_iter())
+    let probs: Vec<f64> = n
+        .par_iter()
+        .zip(k.par_iter())
+        .zip(p.par_iter())
         .map(|((&n_i, &k_i), &p_i)| {
-            let result = ln_gamma(k_i + n_i) - ln_gamma(k_i + 1.) - ln_gamma(n_i) + k_i * (1. - p_i).ln() + n_i * p_i.ln();
+            let result = ln_gamma(k_i + n_i) - ln_gamma(k_i + 1.) - ln_gamma(n_i)
+                + k_i * (1. - p_i).ln()
+                + n_i * p_i.ln();
             result
         })
         .collect();
-    
+
     Ok(probs)
 }
 
 #[pyfunction]
-fn bb(k: PyReadonlyArray1<'_, f64>, n: PyReadonlyArray1<'_, f64>, a: PyReadonlyArray1<'_, f64>, b: PyReadonlyArray1<'_, f64>) -> PyResult<Vec<f64>> {
-    // NB export RAYON_NUM_THREADS=12
+fn bb(
+    k: PyReadonlyArray1<'_, f64>,
+    n: PyReadonlyArray1<'_, f64>,
+    a: PyReadonlyArray1<'_, f64>,
+    b: PyReadonlyArray1<'_, f64>,
+) -> PyResult<Vec<f64>> {
+
     let k = k.to_vec()?;
     let n = n.to_vec()?;
     let a = a.to_vec()?;
     let b = b.to_vec()?;
 
-    let probs: Vec<f64> = n.par_iter().zip(k.par_iter()).zip(a.par_iter()).zip(b.par_iter())
+    let probs: Vec<f64> = n
+        .par_iter()
+        .zip(k.par_iter())
+        .zip(a.par_iter())
+        .zip(b.par_iter())
         .map(|(((&n_i, &k_i), &a_i), &b_i)| {
-            ln_gamma(n_i + 1.) - ln_gamma(k_i + 1.) - ln_gamma(n_i - k_i + 1.) + ln_gamma(k_i + a_i) + ln_gamma(n_i - k_i + b_i) - ln_gamma(n_i + a_i + b_i) + ln_gamma(a_i + b_i) - ln_gamma(a_i) - ln_gamma(b_i);
+            let result = ln_gamma(n_i + 1.)
+	        - ln_gamma(k_i + 1.)
+		- ln_gamma(n_i - k_i + 1.)
+                + ln_gamma(k_i + a_i)
+                + ln_gamma(n_i - k_i + b_i)
+                - ln_gamma(n_i + a_i + b_i)
+                + ln_gamma(a_i + b_i)
+                - ln_gamma(a_i)
+                - ln_gamma(b_i);
+            result
         })
         .collect();
-    
+
     Ok(probs)
 }
 
 #[pymodule]
-#[pyo3(name="core")]
+#[pyo3(name = "core")]
 fn core(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(nb, m)?)?;
     m.add_function(wrap_pyfunction!(bb, m)?)?;
